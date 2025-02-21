57d7855c02b9f93e2a93e288b6d1941d48eec24f
Caleb224 <caleb.beere@gmail.com>
2025-02-21 20:42:43 +0000

    Working Bucket Upload

---
+++ app/api/uploadFile/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse} from "next/server";
+
+import {
+    S3Client,
+    PutObjectCommand
+} from "@aws-sdk/client-s3";
+
+const Bucket = "cloudcomputingproject2025";
+const s3 = new S3Client({
+    region: 'us-east-1',
+    credentials: {
+        accessKeyId: "AKIA5WXWYDF4ATFPC3RG",
+        secretAccessKey: "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"
+    }
+})
+
+export async function POST(request: NextRequest) {
+    const formData = await request.formData();
+    const file = formData.getAll("file") as File[];
+
+    const response = await Promise.all(
+        file.map(async (file: File) => {
+            const Body = (await file.arrayBuffer()) as unknown as Buffer;
+            s3.send(new PutObjectCommand({Bucket, Key: file.name, Body}));
+        })
+    )
+
+    return NextResponse.json(response);
+}
